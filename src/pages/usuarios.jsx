f8aad3532eb81e9d2fba1a11d518a251104cd068
suldegit0429 <migithub2025@gmail.com>
2026-05-01 23:04:40 -0500

    Mejoras visuales CRUD usuarios

--- src/pages/usuarios.jsx
+++ src/pages/usuarios.jsx
@@ -1,26 +1,97 @@
 import React, { useEffect, useState } from "react";
 import { getUsuarios } from "../services/usuarioService";
+import "../App.css";
 
 const Usuarios = () => {
+
+  // ESTADOS
   const [usuarios, setUsuarios] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [editando, setEditando] = useState(null);
 
-  useEffect(() => {
-    cargarUsuarios();
-  }, []);
-
+  // FUNCIONES
   const cargarUsuarios = async () => {
     const data = await getUsuarios();
     setUsuarios(data);
   };
 
+  const agregarUsuario = () => {
+    if (editando) {
+      const usuariosActualizados = usuarios.map((u) =>
+        u.id === editando ? { ...u, nombre, email } : u
+      );
+      setUsuarios(usuariosActualizados);
+      setEditando(null);
+    } else {
+      const nuevoUsuario = {
+        id: Date.now(),
+        nombre,
+        email
+      };
+      setUsuarios([...usuarios, nuevoUsuario]);
+    }
+
+    setNombre("");
+    setEmail("");
+  };
+
+  const eliminarUsuario = (id) => {
+    const nuevosUsuarios = usuarios.filter((u) => u.id !== id);
+    setUsuarios(nuevosUsuarios);
+  };
+
+  const editarUsuario = (usuario) => {
+    setNombre(usuario.nombre);
+    setEmail(usuario.email);
+    setEditando(usuario.id);
+  };
+
+  // EFFECT
+  useEffect(() => {
+    cargarUsuarios();
+  }, []);
+
+  // RETURN
   return (
     <div>
       <h2>CRUD de Usuarios</h2>
 
+      <input
+        type="text"
+        placeholder="Nombre"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      />
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button className="agregar" onClick={agregarUsuario}>
+         {editando ? "Guardar" : "Agregar"}
+
+      </button>
+      
+
       <ul>
-        {usuarios.map((usuario, index) => (
-          <li key={index}>
-            {usuario.nombre} - {usuario.email}
+        {usuarios.map((usuario) => (
+          <li key={usuario.id} className="item-usuario">
+            <span>
+             {usuario.nombre} - {usuario.email}
+            </span>
+
+            <div className="acciones">
+              <button className="btn-editar" onClick={() => editarUsuario(usuario)}>
+              ✏️
+              </button>
+
+               <button className="btn-eliminar" onClick={() => eliminarUsuario(usuario.id)}>
+               ❌
+              </button>
+            </div>
           </li>
         ))}
       </ul>
